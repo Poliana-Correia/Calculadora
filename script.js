76f0cf7f0287f5de3748f96b7bc061ac26bf5ae3
poliana <poliana.karenn05@gmail.com>
2022-01-27 18:36:52 -0300

    finalizando as funções

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function insert (num) {
+function insert(num) {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back() {
+    var limpar = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = limpar.substring(0, limpar.length -1);
+}
+
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = ""
+    }
 }
